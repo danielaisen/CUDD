8555810084ac52a3ab4a23503f154e8e65107295
Daniel Bachmann Aisen <danielmaxaisen@gmail.com>
2024-06-05 19:01:03 +0200

    package.swift updated

--- Package.swift
+++ Package.swift
@@ -3,29 +3,15 @@ import PackageDescription
 
 let package = Package(
     name: "CUDD",
-    dependencies: [
-        .package(url: "https://github.com/ltentrup/CCUDD.git", from: "1.0.0")
-    ]
-)
-
-/*
-
-// swift-tools-version:5.3
-import PackageDescription
-
-let package: Package = Package(
-    name: "CUDD",
     products: [ .library(name: "CUDD", targets: ["CUDD"]),],
     dependencies: [
-        .package(url: "https://github.com/danielaisen/CCUDD.git", from: "1.0.0")
+        .package(url: "https://github.com/ltentrup/CCUDD.git", from: "1.0.0")
     ],
-    targets: [
+    targets: [        
         .target(
-            name: "CUDD",
-            dependencies: []),
+            name: "CUDD", 
+            dependencies: ["CCUDD"]),
         .testTarget(
-            name: "CUDDTests",
-            dependencies: ["CUDD"]),
-    ]
+            name: "CUDDTests", 
+            dependencies: ["CUDD"])]
 )
-*/
